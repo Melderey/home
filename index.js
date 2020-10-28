59de8bf060fcad36144906abb2835479e9b8bc16
Melderey <mimeev2010@gmail.com>
2020-10-28 19:31:33 +0500

    add example

--- index.js
+++ index.js
@@ -1,1 +1,27 @@
-console.log('Test');
+// Урок 13. JavaScript. Все о Map, Set, WeakMap, WeakSet с примерами
+
+// Map
+
+const obj = {
+  name: 'Misha',
+  age: 30,
+};
+
+const entries = [
+  ['name', 'Misha'],
+  ['age', 26],
+];
+
+// console.log(Object.entries(obj));
+// console.log(Object.fromEntries(entries));
+
+const exampleMap = new Map(entries);
+// console.log(exampleMap);
+// console.log(exampleMap.get('name'));
+
+exampleMap
+  .set('newField', 42)
+  .set(obj, 'Value of object');
+// console.log(exampleMap.get(obj));
+exampleMap.delete('age');
+console.log(exampleMap);
